f41fc6affb927d7352f8d684731fdc17b31037c4
Jeferson Guedes <jefersonguedes89@gmail.com>
2025-09-02 12:31:43 -0300

    adicionado o método post

---
+++ src/main/java/com/javanauta/agendadortarefas/business/TarefasService.java
@@ -0,0 +1,38 @@
+package com.javanauta.agendadortarefas.business;
+
+
+import com.javanauta.agendadortarefas.business.dto.TarefasDTO;
+import com.javanauta.agendadortarefas.business.mapper.TarefasConverter;
+import com.javanauta.agendadortarefas.infrastructure.entity.TarefasEntity;
+import com.javanauta.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+import com.javanauta.agendadortarefas.infrastructure.repository.TarefasRepository;
+import com.javanauta.agendadortarefas.infrastructure.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class TarefasService {
+
+    private final TarefasRepository tarefasRepository;
+    private final TarefasConverter tarefaConverter;
+    private final JwtUtil jwtUtil;
+
+    public TarefasDTO gravarTarefa(String token, TarefasDTO tarefasDTO){
+        String email = jwtUtil.extractUsername(token.substring(7));
+
+
+        tarefasDTO.setDataCriacao(LocalDateTime.now());
+        tarefasDTO.setStatusNotificacaoEnum(StatusNotificacaoEnum.PENDENTE);
+        tarefasDTO.setEmailUsuario(email);
+
+
+        TarefasEntity tarefasEntity = tarefaConverter.paraTarefaEntity(tarefasDTO);
+
+        return tarefaConverter.paraTarefaDTO(tarefasRepository.save(tarefasEntity));
+    }
+
+
+}
---
+++ src/main/java/com/javanauta/agendadortarefas/business/mapper/TarefasConverter.java
@@ -0,0 +1,11 @@
+package com.javanauta.agendadortarefas.business.mapper;
+
+import com.javanauta.agendadortarefas.business.dto.TarefasDTO;
+import com.javanauta.agendadortarefas.infrastructure.entity.TarefasEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TarefasConverter {
+    TarefasEntity paraTarefaEntity(TarefasDTO tarefasDTO);
+    TarefasDTO paraTarefaDTO(TarefasEntity tarefasEntity);
+}
